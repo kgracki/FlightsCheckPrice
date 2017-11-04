9698d99fc3b3d955adb93ab913f10a860c8c4077
Kacper <k@ble.pl>
2017-11-04 14:25:19 +0100

    Working collecting date and price data

--- src/flights_check_price.py
+++ src/flights_check_price.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
+# url address with specific flight information 
 url = ("http://www.azair.com/azfin.php?tp=0&"
        "searchtype=flexi&srcAirport=Warsaw+%"
        "5BWAW%5D+%28%2BWMI%2CLCJ%2CLUZ%2CBZG"
@@ -20,41 +21,22 @@ url = ("http://www.azair.com/azfin.php?tp=0&"
        "true&dep3=true&dep4=true&dep5=true&dep6=true&arr0"
        "=true&arr1=true&arr2=true&arr3=true&arr4=true&arr"
        "5=true&arr6=true&maxChng=0&isOneway=return&resultSubmit=Search")
+
+# get request from website
 page = requests.get(url)
 print page
-#payload = {'key1': 'Warsaw Chopin', 'key2': 'Billund'}
-#r = requests.post(url, data=payload)
-#print r
-#print page
+
+# parse specific html page
 soup = BeautifulSoup(page.content, 'html.parser')
 
+# find results by using CSS selector
+# and collect arrays of data
 g_list = soup.findAll("div", {"class": "result"})
-date_g_list = soup.findAll("span", {"class": "date"})
-for element in date_g_list:
-    print element.text
-price_g_list = soup.findAll("div", {"class": "totalPrice"})
-for element in price_g_list:
-    tp = element.find("span", {"class": "tp"})
-    print tp.text
+date_g_list = soup.findAll("span", {"class": "date"}) 
+price_g_list = soup.findAll("span", {"class": "tp"})
 
-for element in g_list:
-    date = element.findAll("span", {"class", "date"})
-    from_place = element.findAll("span", {"class": "from"})
-    to_place = element.findAll("span", {"class": "to"})
-    detail = element.find("div", {"class": "sumPrice"})
-    totPrice = element.find("span", {"class": "bp"})
-    #subPrice2 = element.find("span", {"class": "subPrice"})
-    #legPrice = element.find("span", {"class": "legPrice"})
-    #length = element.find("span", {"class": "lengthOfStay"})
-
-    print date[0].text
-    print from_place[0].text
-    print to_place[0].text
-    print detail
-    print totPrice
-    #print subPrice1.text
-    #print subPrice2.text
-    #print legPrice.text
-    #print length
-    
-#print g_data
+# print data
+for element in range(len(g_list)):
+    print date_g_list[element * 2].text
+    print date_g_list[(element * 2) + 1].text
+    print price_g_list[element].text
